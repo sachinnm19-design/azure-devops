25e920b09bfc528f201c14c2482946e5e4f9205c
sachinnm19-design <sachinnm19@gmail.com>
2026-02-11 14:55:30 +0000

    Clean up datasources.tf by removing unused data
    Removed unused resource provider registration data blocks.

--- infra/datasources.tf
+++ infra/datasources.tf
@@ -1,12 +1,3 @@
 data "azurerm_client_config" "current" {}
 
 data "azurerm_subscription" "current" {}
-
-# Check resource provider registration status
-data "azurerm_resource_provider_registration" "compute" {
-  name = "Microsoft.Compute"
-}
-
-data "azurerm_resource_provider_registration" "container_registry" {
-  name = "Microsoft.ContainerRegistry"
-}
