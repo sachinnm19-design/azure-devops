7136d6f5a35e5328b9b00e82009234eaa3f82639
sachinnm19-design <sachinnm19@gmail.com>
2026-02-11 15:02:45 +0000

    Add new variables for Azure deployment configuration

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,52 @@
-# ... (keep existing variables) ...
+variable "environment" {
+  type        = string
+  description = "The name of the environment for the deployment (e.g., 'dev', 'staging', 'prod')."
+}
+
+variable "location" {
+  type        = string
+  description = "The Azure region where resources will be deployed (e.g., 'eastus', 'westus2')."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the Azure Resource Group where all resources will be created."
+}
+
+variable "acr_name" {
+  type        = string
+  description = "The name of the Azure Container Registry to be created or referenced."
+}
+
+variable "app_service_plan_name" {
+  type        = string
+  description = "The name of the App Service Plan for deploying the web application."
+}
+
+variable "webapp_name" {
+  type        = string
+  description = "The name of the Azure Web App to be created within the App Service Plan."
+}
+
+variable "sku_name" {
+  type        = string
+  description = "The SKU name for the App Service Plan (e.g., 'B1', 'P1V2'). Determines performance and pricing."
+}
+
+variable "image_name" {
+  type        = string
+  description = "The name of the container image to be deployed in the Azure Web App."
+}
+
+variable "image_tag" {
+  type        = string
+  description = "The tag used to identify the version of the container image (e.g., 'latest', 'v1.0.0')."
+}
+
+variable "sp_object_id" {
+  type        = string
+  description = "Object ID of the Service Principal used by Terraform"
+}
 
 variable "ip_restrictions" {
   type = list(object({
