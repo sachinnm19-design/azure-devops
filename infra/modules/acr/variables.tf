706fd9ae35e8a26f28116d09fd4244526b9c8513
sachinnm19-design <sachinnm19@gmail.com>
2026-02-11 16:55:42 +0000

    Enhance variable definitions in variables.tf
    Added descriptions for variables and updated SKU default to Premium with validation.

--- infra/modules/acr/variables.tf
+++ infra/modules/acr/variables.tf
@@ -1,26 +1,37 @@
 variable "acr_name" {
-  type = string
+  type        = string
+  description = "Name of the Azure Container Registry"
 }
 
 variable "resource_group_name" {
-  type = string
+  type        = string
+  description = "Name of the resource group"
 }
 
 variable "location" {
-  type = string
+  type        = string
+  description = "Azure region"
 }
 
 variable "sku" {
-  type    = string
-  default = "Basic"
+  type        = string
+  description = "SKU for ACR (Basic, Standard, Premium)"
+  default     = "Premium"
+  
+  validation {
+    condition     = contains(["Basic", "Standard", "Premium"], var.sku)
+    error_message = "SKU must be Basic, Standard, or Premium"
+  }
 }
 
 variable "public_access_enabled" {
-  type    = bool
-  default = false
+  type        = bool
+  description = "Enable public network access (only works with Premium SKU)"
+  default     = false
 }
 
 variable "tags" {
-  type    = map(string)
-  default = {}
+  type        = map(string)
+  description = "Tags to apply to the ACR"
+  default     = {}
 }
