b235156b89c80c46bd943c4982af83fcb59ca10f
sachinnm19-design <sachinnm19@gmail.com>
2026-02-11 12:43:27 +0000

    Add variables for ACR module configuration

---
+++ modules/acr/variable.tf
@@ -0,0 +1,26 @@
+variable "acr_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "sku" {
+  type    = string
+  default = "Basic"
+}
+
+variable "public_access_enabled" {
+  type    = bool
+  default = false
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
