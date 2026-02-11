cda02429bab95631234e2963c2363056241de53a
sachinnm19-design <sachinnm19@gmail.com>
2026-02-11 17:23:13 +0000

    Update ACR configuration to disable public access

--- infra/modules/acr/main.tf
+++ infra/modules/acr/main.tf
@@ -1,12 +1,13 @@
 resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  sku                 = var.sku
-  admin_enabled       = false
+  name                          = var.acr_name
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
+  sku                           = var.sku
+  admin_enabled                 = false
+  public_network_access_enabled = false
 
-  # Disable public access (requires Premium SKU)
-  public_network_access_enabled = var.public_access_enabled
+  # Allow Azure services and resources to access this registry
+  network_rule_bypass_option = "AzureServices"
 
   tags = var.tags
 }
