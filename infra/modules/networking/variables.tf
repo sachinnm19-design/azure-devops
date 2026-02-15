08dc0661fb13f43dc74829e962242e999e212ec7
sachinnm19-design <sachinnm19@gmail.com>
2026-02-15 20:34:19 +0000

    Reorganize and add variables for networking module

--- infra/modules/networking/variables.tf
+++ infra/modules/networking/variables.tf
@@ -1,21 +1,44 @@
 variable "resource_prefix" {
-  type = string
-}
-
-variable "location" {
-  type = string
+  type        = string
+  description = "Prefix for all resources"
 }
 
 variable "resource_group_name" {
-  type = string
+  type        = string
+  description = "Name of the resource group"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region"
 }
 
 variable "create_nsg" {
-  type    = bool
-  default = true
+  type        = bool
+  description = "Whether to create Network Security Group"
+  default     = true
+}
+
+variable "vnet_address_space" {
+  type        = string
+  description = "Address space for VNet"
+  default     = "10.0.0.0/16"
+}
+
+variable "app_service_subnet_prefix" {
+  type        = string
+  description = "Address prefix for App Service subnet"
+  default     = "10.0.1.0/24"
+}
+
+variable "private_endpoint_subnet_prefix" {
+  type        = string
+  description = "Address prefix for private endpoints subnet"
+  default     = "10.0.2.0/24"
 }
 
 variable "tags" {
-  type    = map(string)
-  default = {}
+  type        = map(string)
+  description = "Tags to apply to resources"
+  default     = {}
 }
