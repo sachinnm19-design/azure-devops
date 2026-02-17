c3b4c231ffce3a60264633e0b1647e3ea6629202
sachinnm19-design <sachinnm19@gmail.com>
2026-02-17 18:07:06 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,18 +1,26 @@
 from flask import Flask, jsonify
+import logging
+import os
+from azure.monitor.opentelemetry import configure_azure_monitor
+
+# Enable Azure Application Insights
+configure_azure_monitor()
 
 app = Flask(__name__)
 
+# Basic logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @app.route("/health")
 def health():
-    return jsonify({"status": "ok"})
-
+    logger.info("Health endpoint called")
+    return jsonify({"status": "ok"}), 200
 
 @app.route("/")
 def home():
-    return "Hello from Azure Web App for Containers via GitHub Actions & Terraform!"
-
+    logger.info("Home endpoint called")
+    return "Hello from Azure Web App for Containers!"
 
 if __name__ == "__main__":
-    # Runs on port 3000 and listens on all interfaces
     app.run(host="0.0.0.0", port=3000)
