421453c2c060d8530c48a7d10122cbd3f3cf9e8e
sachinnm19-design <sachinnm19@gmail.com>
2026-02-17 20:05:33 +0000

    Refactor variable definitions in variables.tf
    Rearranged variable definitions and removed descriptions for some variables.

--- infra/modules/networking/variables.tf
+++ infra/modules/networking/variables.tf
@@ -1,44 +1,21 @@
 variable "resource_prefix" {
-  type        = string
-  description = "Prefix for all resources"
-}
-
-variable "resource_group_name" {
-  type        = string
-  description = "Name of the resource group"
+  type = string
 }
 
 variable "location" {
-  type        = string
-  description = "Azure region"
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
 }
 
 variable "create_nsg" {
-  type        = bool
-  description = "Whether to create Network Security Group"
-  default     = true
-}
-
-variable "vnet_address_space" {
-  type        = string
-  description = "Address space for VNet"
-  default     = "10.0.0.0/16"
-}
-
-variable "app_service_subnet_prefix" {
-  type        = string
-  description = "Address prefix for App Service subnet"
-  default     = "10.0.1.0/24"
-}
-
-variable "private_endpoint_subnet_prefix" {
-  type        = string
-  description = "Address prefix for private endpoints subnet"
-  default     = "10.0.2.0/24"
+  type    = bool
+  default = true
 }
 
 variable "tags" {
-  type        = map(string)
-  description = "Tags to apply to resources"
-  default     = {}
+  type    = map(string)
+  default = {}
 }
