ff8c7b8565acbb43e02af1cced209144433370b3
sachinnm19-design <sachinnm19@gmail.com>
2026-02-18 16:42:33 +0000

    Delete infra/modules/networking directory

--- infra/modules/networking/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "nsg_id" {
-  value = var.create_nsg ? azurerm_network_security_group.nsg[0].id : null
-}
-
-output "nsg_name" {
-  value = var.create_nsg ? azurerm_network_security_group.nsg[0].name : null
-}
